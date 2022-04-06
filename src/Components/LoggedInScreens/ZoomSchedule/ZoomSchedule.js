22e8cdc6fb5b30b18e1dd548c1e0a4b083037144
THAMIZH <thamizharasan2373@gmail.com>
2022-04-06 20:55:46 +0530

    schdule api integrated

--- src/Components/LoggedInScreens/ZoomSchedule/ZoomSchedule.js
+++ src/Components/LoggedInScreens/ZoomSchedule/ZoomSchedule.js
@@ -1,9 +1,58 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+
 import "./Style/ZoomScheduleStyle.scss";
 import { PageTitlebar } from "./../PageTitlebar/PageTitlebar";
-import { TiTick } from "react-icons/ti";
+import { TiTick, TiVideo } from "react-icons/ti";
+import axios from "../../../index";
+import { useParams } from "react-router-dom";
 
 export function ZoomSchedule() {
+  const mL = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December",
+  ];
+  const mS = [
+    "Jan",
+    "Feb",
+    "Mar",
+    "Apr",
+    "May",
+    "June",
+    "July",
+    "Aug",
+    "Sept",
+    "Oct",
+    "Nov",
+    "Dec",
+  ];
+  const [allSchedule, setAllSchedule] = useState([]);
+  const param = useParams();
+  const token = localStorage.getItem("token");
+
+  function getAllSchedule() {
+    axios
+      .post("/schedule/view/" + param.courseId, { token: token })
+      .then((res) => {
+        console.log(res.data);
+        setAllSchedule(res.data);
+      })
+      .catch((error) => {
+        console.log(error.message);
+      });
+  }
+  useEffect(() => {
+    getAllSchedule();
+  }, [param.courseId]);
   return (
     <div className="zoomSchedule">
       <PageTitlebar emoji="" title={"Zoom Schedule"} />
@@ -17,6 +66,42 @@ export function ZoomSchedule() {
         <div className="zoomSchedule-viewSyllabus">View Syllabus</div>
       </div>
 
+      {allSchedule.map((schedule, key) => {
+        let dateTime = new Date(schedule.classDate);
+        let date = dateTime.getDate();
+        let month = dateTime.getMonth();
+
+        return (
+          <div key={key} className="zoomSchedule-Card">
+            <div className="zoomSchedule-Card-Left">
+              <p>{schedule.batchTime} PM</p>
+              <p>{date}</p>
+              <p>{mL[month]}</p>
+            </div>
+            <div className="zoomSchedule-Card-Right-Row">
+              <div className="zoomSchedule-Card-Right-Div1">
+                <p>{schedule.topic}</p>
+                <p>Zoom Meeting with {schedule.trainer.trainerName}</p>
+                <p>60 Minutes</p>
+              </div>
+              {schedule.isCompleted ? (
+                <div className="zoomSchedule-Card-Right-Div2">
+                  <TiTick className="zoomSchedule-Card-Right-Div2-icons" />
+                  <p>Completed</p>
+                </div>
+              ) : (
+                <div className="zoomSchedule-Card-Right-Div2">
+                  <TiVideo className="zoomSchedule-Card-Right-Div2-icons" />
+                  <p>Join</p>
+                </div>
+              )}
+
+              <div className="zoomSchedule-Card-Right-Div3"></div>
+            </div>
+          </div>
+        );
+      })}
+      {/* 
       <div className="zoomSchedule-Card">
         <div className="zoomSchedule-Card-Left">
           <p>10:00 AM</p>
@@ -155,7 +240,7 @@ export function ZoomSchedule() {
           </div>
           <div className="zoomSchedule-Card-Right-Div3"></div>
         </div>
-      </div>
+      </div> */}
     </div>
   );
 }
